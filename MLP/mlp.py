b050c2fb3150f5d61f7496ab86f5bfbd2bfbc7f4
Omar Essam <omar_2412@live.com>
2016-03-17 18:06:03 +0200

    Different activations

--- MLP/mlp.py
+++ MLP/mlp.py
@@ -2,25 +2,30 @@ import theano
 import theano.tensor as T
 import numpy as np
 import matplotlib.pyplot as plt
-
+from six.moves import cPickle
 
 # Declare constants
-HIDDEN_NEURONS = 100
+HIDDEN_NEURONS = 50
 NUM_CLASSES = 10
 NUM_FEATURES = 0
-LEARNING_RATE = 0.05
-XVAL_SIZE = 0.3
-NUM_EPOCHS = 5
+LEARNING_RATE = 0.04
+XVAL_SIZE = 0.1
+NUM_EPOCHS = 10
 
 # Load Data
-Training_set = np.loadtxt("/media/omarito/DATA/Data Sets/MNIST/train.csv",
-                          dtype=np.uint8, skiprows=1, delimiter=",")
+# Training_set = np.loadtxt("/media/omarito/DATA/Data Sets/MNIST/train.csv",
+#                           dtype=np.uint8, skiprows=1, delimiter=",")
 
-Test_set = np.loadtxt("/media/omarito/DATA/Data Sets/MNIST/mnist_test.csv",
-                      dtype=np.uint8, skiprows=1, delimiter=",")
+# Test_set = np.loadtxt("/media/omarito/DATA/Data Sets/MNIST/mnist_test.csv",
+#                       dtype=np.uint8, skiprows=1, delimiter=",")
+
+Training_set = cPickle.load(open("train.pickle", "rb"))
+
+Test_set = cPickle.load(open("test.pickle", "rb"))
 
 # Training Data
 X_Train = Training_set[XVAL_SIZE * Training_set.shape[0]:, 1:]
+X_Train = (X_Train - X_Train.mean()) / X_Train.std()
 X_Train = np.c_[np.ones((X_Train.shape[0], 1)), X_Train]
 Y_Train = Training_set[XVAL_SIZE * Training_set.shape[0]:, 0]
 Y_Train_onehot = np.zeros((Y_Train.shape[0], NUM_CLASSES))
@@ -49,10 +54,6 @@ t = T.dmatrix('Target Values')
 Netj = T.dmatrix('Net of hidden layer')
 Netk = T.dmatrix('Net of output layer')
 Aj = T.dmatrix('Activation of hidden layer')
-# Wj = theano.shared(value=np.random.rand(NUM_FEATURES, HIDDEN_NEURONS),
-#                    name="Weights of hidden layer")
-# Wk = theano.shared(value=np.random.rand(HIDDEN_NEURONS, NUM_CLASSES),
-#                    name="Weights of output layer")
 Wj = np.random.rand(NUM_FEATURES, HIDDEN_NEURONS)
 Wk = np.random.rand(HIDDEN_NEURONS, NUM_CLASSES)
 Weights = theano.shared(value=np.concatenate((Wj.ravel(), Wk.ravel()), axis=0),
@@ -61,6 +62,8 @@ Weights = theano.shared(value=np.concatenate((Wj.ravel(), Wk.ravel()), axis=0),
 # Define equations
 Netj = T.dot(x, Weights[0:NUM_FEATURES * HIDDEN_NEURONS]
              .reshape((NUM_FEATURES, HIDDEN_NEURONS)))
+# Aj = T.maximum(Netj, 0)
+# Aj = T.tanh(Netj)
 Aj = T.nnet.sigmoid(Netj)
 
 Netk = T.dot(Aj, Weights[NUM_FEATURES * HIDDEN_NEURONS:]
@@ -92,4 +95,4 @@ plt.show()
 Test_Result = np.argmax(forwardProp(X_Test), axis=1)
 Score = float(len(np.where(Test_Result != Y_Test)[0])) / float(
     (Y_Test.shape[0])) * 100
-print "The model performed with an accuracy of: %.2f%" % (str(Score)) + "%"
+print "The model performed with an accuracy of: %.2f" % (float(Score)) + "%"
