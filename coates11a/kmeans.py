0cc181c4f6635695e3007b1cbfbf51d42d70a3a0
Omar Essam <omar_2412@live.com>
2016-05-20 14:11:43 +0200

    Fixed Coates11a

--- coates11a/kmeans.py
+++ coates11a/kmeans.py
@@ -4,60 +4,70 @@ import numpy as np
 from six.moves import cPickle
 from sklearn.cluster import MiniBatchKMeans
 from sklearn.svm import SVC
-import skimage.util.shape as utl
-import sklearn.decomposition as DCM
 import sklearn.preprocessing as pre
 
-NUM_FEATURES = 75
-NUM_PATCHES = 400000
-PATCH_SIZE = (8, 8, 1)
-WHITENING = False
+NUM_FEATURES = 500
+NUM_PATCHES = 500000
+PATCH_SIZE = (7, 7, 1)
+WHITENING = True
 IMAGE_DIMS = (28, 28, 1)
 
 
-def get_distances(x, trainclusters):
-    """Retrieve the triangular K-means mapping."""
-    z = trainclusters.transform(x)
-    meanz = z.mean(axis=1)
-    meanz = meanz.reshape((-1, 1)).repeat(NUM_FEATURES, axis=1)
-    distances = meanz - z
-    distances = np.maximum(0, distances)
-    return distances
+def normalize(dataset):
+    """Brightness and Contrast Normalization."""
+    dataset_mean = dataset.mean(axis=1)
+    dataset_std = dataset.std(axis=1)
+    dataset -= dataset_mean.reshape((-1, 1)).repeat(dataset.shape[1], axis=1)
+    dataset /= (dataset_std.reshape((-1, 1)).repeat(
+                dataset.shape[1], axis=1) + 10)
+    return dataset
 
 
-def get_patches(images):
-    """Transform each image into n patches."""
-    images = images.reshape((-1, IMAGE_DIMS[0], IMAGE_DIMS[1], IMAGE_DIMS[2]))
-    images = utl.view_as_windows(images,
-                                (images.shape[0],
-                                PATCH_SIZE[0],
-                                PATCH_SIZE[1],
-                                PATCH_SIZE[2]
-                                ),
-                                step=4
-                                )
-    images = images.reshape((images.shape[1] * images.shape[2] * images.shape[4],
-                            np.prod(PATCH_SIZE)
-                            ))
-    print "Done transforming images"
-    return images
+def whiten(dataset):
+    """Perform ZCA whitening."""
+    dataset = dataset.astype("float32")
+    m = dataset.mean(axis=0)
+    dataset -= m
+    cov = np.cov(dataset.T) / dataset.shape[0]
+    U, S, V = np.linalg.svd(cov)
+    components = np.dot(np.dot(U, np.diag(1 / np.sqrt(S + 10**-5))), U.T)
+    dataset = np.dot(dataset, components.T)
+    return dataset, m, components
+
+
+def extract_features(data, trainclusters, m=None, p=None):
+    """Extract patches from data and pool."""
+    print "Extracting features"
+    num_ps = data.shape[1] / np.prod(PATCH_SIZE)
+    xc = np.zeros((data.shape[0], num_ps * NUM_FEATURES))
+    for i in range(data.shape[0]):
+        patches = list()
+        for j in range(0, np.prod(IMAGE_DIMS), np.prod(PATCH_SIZE)):
+            patch = data[i, j:j + np.prod(PATCH_SIZE)]
+            if(WHITENING):
+                patch -= m
+                patch = np.dot(patch, p.T)
+            patch = trainclusters.transform(patch.reshape((1, -1)))
+            mean = patch.mean(axis=1)
+            activation = np.maximum(0, mean - patch)
+            patches.append(activation)
+        patches = np.array(patches)
+        patches = patches.reshape((1, -1))
+        patches = normalize(patches)
+        xc[i] = patches
+    return xc
+
 
 # Read CIFAR-10
 print "Reading Data and normalizing"
 mnist = cPickle.load(open("train.pickle", "rb"))
-
+scaler = pre.StandardScaler(copy=False)
 # Load the training data
-X_Train = mnist[:, 1:].astype("float32")
+X_Train = mnist[:, 1:].astype("float32")[0:100]
 
-Y_Train = mnist[:, 0]
-
-# Whiten input
-if(WHITENING):
-    print "Whitening data"
-    pca = DCM.PCA(whiten=True, copy=False)
-    pca.fit(X_Train)
-    X_Train = pca.transform(X_Train)
-    del pca
+Y_Train = mnist[:, 0][0:100]
+M = None
+P = None
 
 # Extract random patches
 print "Extracting random patches"
@@ -65,56 +75,53 @@ patches = list()
 for i in range(NUM_PATCHES):
     r = np.random.randint(0, IMAGE_DIMS[0] - PATCH_SIZE[0] + 1, 1)[0]
     c = np.random.randint(0, IMAGE_DIMS[1] - PATCH_SIZE[0] + 1, 1)[0]
-    patch = X_Train[i % X_Train.shape[0], :].reshape((IMAGE_DIMS[0], IMAGE_DIMS[1], IMAGE_DIMS[2]))
+    patch = X_Train[i % X_Train.shape[0], :].reshape(IMAGE_DIMS)
     patch = patch[r:r + PATCH_SIZE[0], c:c + PATCH_SIZE[1], :]
     patches.append(patch.ravel())
-    del patch
 
 patches = np.array(patches).astype("float32")
-# scaler = pre.StandardScaler(copy=False)
-# patches = scaler.fit_transform(patches)
+print "Normalizing patches"
+patches = normalize(patches)
 
-# Normalize
-# X_Train = scaler.fit_transform(X_Train)
+if(WHITENING):
+    print "Whitening patches"
+    patches, M, P = whiten(patches)
 
 print "Training K-means"
 # BEGIN FEATURE EXTRACTION #
 TrainClusters = MiniBatchKMeans(n_clusters=NUM_FEATURES, max_iter=1000,
-                                batch_size=10000)
+                                batch_size=25000)
 TrainClusters.fit(patches)
+del patches
 
-Distances = get_distances(get_patches(
-    X_Train.reshape((-1, IMAGE_DIMS[0], IMAGE_DIMS[1], IMAGE_DIMS[2]))), TrainClusters).reshape(
-    (X_Train.shape[0], -1))
-
-# Distances = scaler.fit_transform(Distances)
+# Extract features
+Distances = extract_features(X_Train, TrainClusters, M, P)
+Distances = np.concatenate((Distances, np.ones(
+    (Distances.shape[0], 1))), axis=1)
+# Standardize
+Distances = scaler.fit_transform(Distances)
 
 # BEGIN CLASSIFIER TRAINING #
 print "Training the SVM"
-clf = SVC(kernel="linear", max_iter=1, C=100)
+clf = SVC(kernel="linear", max_iter=50, C=50)
 clf.fit(Distances, Y_Train)
 
 print "Testing scores"
-
 # Load test data
 test = cPickle.load(open("test.pickle", "rb"))
 X_Test = test[:, 1:].astype("float32")
 
 Y_Test = test[:, 0]
-del test
-if(WHITENING):
-    pca = DCM.PCA(whiten=True, copy=False)
-    pca.fit(X_Test)
-    X_Test = pca.transform(X_Test)
 
-# Normalize
-# X_Test = scaler.fit_transform(X_Test)
+# Extract features
+Distances = extract_features(X_Test, TrainClusters, M, P)
+Distances = np.concatenate((Distances, np.ones(
+    (Distances.shape[0], 1))), axis=1)
+# Standardize
+Distances = scaler.fit_transform(Distances)
 
-Distances = get_distances(get_patches(
-    X_Test.reshape(
-        (-1, IMAGE_DIMS[0], IMAGE_DIMS[1], IMAGE_DIMS[2]))), TrainClusters).reshape(X_Test.shape[0], -1)
-# Distances = scaler.fit_transform(Distances)
 Test_Result = clf.predict(Distances)
 Score = float(len(np.where(Test_Result == Y_Test)[0])) / float(
     (Y_Test.shape[0])) * 100
-print "The model performed with an accuracy of: %.2f" % (float(Score)) + "%"
+print "The model classified with an accuracy of: %.2f" % (
+    float(Score)) + "%"
